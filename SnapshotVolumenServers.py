9241ac1fade021a22db8d2f9a2c44c3bbdb0d650
Roberto Reyes <roberto.reyes@clicksandbricks.pe>
2015-10-09 02:15:11 -0500

    version 1.1

--- SnapshotVolumenServers.py
+++ SnapshotVolumenServers.py
@@ -3,6 +3,7 @@
 #====================================================
 
 import boto.ec2
+import codecs
 regiones = ["us-east-1","us-west-1","us-west-2","ap-northeast-1","ap-southeast-1","ap-southeast-2","eu-central-1","eu-west-1","sa-east-1"]
 for region in regiones:
 	conn=boto.ec2.connect_to_region(region)	
@@ -13,11 +14,25 @@ for region in regiones:
 				if str(inst.tags['Use']) == 'bd':					
 					volumes = conn.get_all_volumes(filters={'attachment.instance-id': inst.id})
 					for vol in volumes:
-						if 'Name' in vol.tags:        
-							if "/var/lib/mysql" in vol.tags['Name']:
-								print "%s (%s) [%s] [%s] [%s]" % (inst.tags['Use'], inst.id, vol.id, vol.tags['Name'], inst.region)
-								#print "%s (%s) [%s] [%s]" % (inst.tags['Use'], inst.id, vol.id, inst.region)
-								description = inst.tags['Name'] + " " + vol.id  + " " + vol.tags['Name']
-								print description
-							#snapshot = conn.create_snapshot()
-							#snapshot.add_tags({'Name': vol.tags['Name'], 'Server': inst.tags['Name']})
+						if 'Name' in vol.tags:
+							if 'Name' in inst.tags:
+								if "/var/lib/mysql" in vol.tags['Name']:
+									descrip = inst.tags['Name']  + " " + vol.id + " " + vol.tags['Name'] + " "  + vol.attach_data.device
+									descripcode = descrip.encode('ascii', 'ignore')
+									volname = vol.tags['Name']
+									volnamecode = volname.encode('ascii', 'ignore')
+									instname = inst.tags['Name']
+									instnamecode = instname.encode('ascii', 'ignore')
+									snapshot = conn.create_snapshot(vol.id, descripcode)
+									snapshot.add_tags({'Name': volnamecode, 'Server': instnamecode})
+									print "%s (%s) [%s]" % (descripcode, snapshot.id , inst.region)
+								if "/var/lib/mongo" in vol.tags['Name']:
+									descrip = inst.tags['Name']  + " " + vol.id + " " + vol.tags['Name'] + " "  + vol.attach_data.device
+									descripcode = descrip.encode('ascii', 'ignore')
+									volname = vol.tags['Name']
+									volnamecode = volname.encode('ascii', 'ignore')
+									instname = inst.tags['Name']
+									instnamecode = instname.encode('ascii', 'ignore')
+									snapshot = conn.create_snapshot(vol.id, descripcode)
+									snapshot.add_tags({'Name': volnamecode, 'Server': instnamecode})
+									print "%s (%s) [%s]" % (descripcode, snapshot.id , inst.region)
